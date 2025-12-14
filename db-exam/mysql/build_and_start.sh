a50f1867f38176dd587479715306d957085c3d1f
seonghun120614 <seonghun120614@gmail.com>
2025-12-14 15:44:37 +0900

    fix:  파일 잘못된 key 수정

--- db-exam/mysql/build_and_start.sh
+++ db-exam/mysql/build_and_start.sh
@@ -4,8 +4,11 @@
 
 set -euo pipefail
 
-THIS=$(basename $0)
+THIS="Mysql CI Script"
+DIRNAME=$(dirname $0)
 IMAGE_NAME="my-mysql-image:1.0"
+
+cd $DIRNAME
 
 log() {
     echo -e "\033[1;32m[ ${THIS} ]\033[0m $1"
@@ -20,6 +23,10 @@ log_command() {
 log_command docker stop $(docker ps -aq -f ancestor=$IMAGE_NAME) 2>/dev/null || true
 log_command docker rmi $IMAGE_NAME 2>/dev/null || true
 log_command docker build -f "Dockerfile.dev" -t $IMAGE_NAME .
-log_command docker run -d --rm -p "3306:3306" -v "./logs:/var/log/mysql" --env-file ".env.dev" $IMAGE_NAME
+log_command docker run -d \
+    --rm -p "3306:3306" \
+    -v "$(pwd)/logs:/var/log/mysql" \
+    --env-file ".env.dev" \
+    $IMAGE_NAME
 
 log "실행 완료"
